b2eaad32dede39a093cd433b61e56018845cc5a3
Rishi Patel <Patel.Rishi@ibm.com>
2023-08-11 15:19:41 +0530

    changed image name

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ git clone https://github.com/rishi-patel1/DatabaseChangeManagement.git
 cp /DatabaseChangeManagement/sql/*.sql $FLYWAY_HOME/sql/
 
 # Perform the rest of your entrypoint commands
-cp -f /var/flyway/data/*.sql  $FLYWAY_HOME/sql/
+# cp -f /var/flyway/data/*.sql  $FLYWAY_HOME/sql/
 ls -a $FLYWAY_HOME/sql/
 $FLYWAY_HOME/flyway baseline migrate info  -user=${DB_USER} -password=${DB_PASSWORD} -url=${DB_URL}
