6d575bbc3fefa80b8729707685879f0921051ff1
Rishi Patel <Patel.Rishi@ibm.com>
2023-08-18 17:16:16 +0530

    added commands

--- Pipeline/scripts/temp.sh
+++ Pipeline/scripts/temp.sh
@@ -2,17 +2,7 @@ echo "Hello World"
 ibm_cloud_login() {
               echo "INFO : Login to IBM Cloud account ... "
               ibmcloud config --check-version false
-              ibm_cloud_login_retry(){
-                ibmcloud login -a https://cloud.ibm.com --apikey rf7_LUzhtxwznAjGK-9ZK6SHuFMaTAHi3uwgVyWJCjDB --no-region; login_status=$?;
-              }
-              for login_retry in {1..3}; do
-                  if [[ $login_status -ne 0 ]]; then
-                    echo "INFO : trying to line $login_retry times in ibm account..."; ibm_cloud_login_retry
-                  else 
-                    echo LOGIN SUCCEEDED. INFO: $login_status
-                    break
-                  fi
-              done
+              ibmcloud login -a https://cloud.ibm.com --apikey rf7_LUzhtxwznAjGK-9ZK6SHuFMaTAHi3uwgVyWJCjDB --no-region; login_status=$?;
               ibmcloud ks cluster config -c $CLUSTER_ID > /artifacts/config_tmp.txt; rm -rf /artifacts/config_tmp.txt
           }
 ibm_cloud_login
@@ -20,6 +10,7 @@ ibm_cloud_login
 cd ../../pg-flyway-db-migration
 echo INFO: inside flyway folder; ls;
 export DB_PASS=$DB_PASS
+cat pg-flyway-job.yaml
 envsubst < pg-flyway-job.yaml > pg-flyway-job.yaml
 cat pg-flyway-job.yaml
 kubectl delete -f pg-flyway-job.yaml       
