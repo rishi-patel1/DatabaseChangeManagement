4b3ff11637b4178427c4ff01282d5dbb7b2f1687
Rishi Patel <Patel.Rishi@ibm.com>
2023-08-18 17:18:27 +0530

    added commands

--- Pipeline/scripts/temp.sh
+++ Pipeline/scripts/temp.sh
@@ -6,12 +6,12 @@ ibm_cloud_login() {
               ibmcloud ks cluster config -c $CLUSTER_ID > /artifacts/config_tmp.txt; rm -rf /artifacts/config_tmp.txt
           }
 ibm_cloud_login
-# kubectl get pods
+kubectl get pods
 cd ../../pg-flyway-db-migration
 echo INFO: inside flyway folder; ls;
 export DB_PASS=$DB_PASS
 cat pg-flyway-job.yaml
 envsubst < pg-flyway-job.yaml > pg-flyway-job.yaml
 cat pg-flyway-job.yaml
-kubectl delete -f pg-flyway-job.yaml       
-kubectl apply -f pg-flyway-job.yaml       
+# kubectl delete -f pg-flyway-job.yaml       
+# kubectl apply -f pg-flyway-job.yaml       
