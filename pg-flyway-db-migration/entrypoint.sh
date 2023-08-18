30608809774d2d2505f76c61e59cc275b69070b2
Rishi Patel <Patel.Rishi@ibm.com>
2023-08-18 16:37:33 +0530

    changed dockerfile

--- pg-flyway-db-migration/entrypoint.sh
+++ pg-flyway-db-migration/entrypoint.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/rishi-patel1/DatabaseChangeManagement.git
 
 # Copy SQL files from cloned repository
-cp /DatabaseChangeManagement/sql/*.sql $FLYWAY_HOME/sql/
+cp /DatabaseChangeManagement/pg-flyway-db-migration/sql/*.sql $FLYWAY_HOME/sql/
 
 # Perform the rest of your entrypoint commands
 # cp -f /var/flyway/data/*.sql  $FLYWAY_HOME/sql/
